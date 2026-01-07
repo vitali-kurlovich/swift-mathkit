b5c2fd06502249d6e34a9393a02551f4daa028a5
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-01-07 20:11:17 +0400

    Update example

--- Example/MathKitApp/MathKitApp/ContentView.swift
+++ Example/MathKitApp/MathKitApp/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TransformEditorView()
-            .padding()
+        TransformEditorView {
+            RoundedRectangle(cornerSize: .init(width: 120, height: 120))
+                .fill(.indigo.secondary)
+                .overlay {
+                    Image(systemName: "heart.circle.fill")
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(.yellow, .green, .red.gradient)
+                        .font(.system(size: 220, weight: .semibold))
+                        .symbolEffect(.breathe)
+                }
+                .padding()
+        }
     }
 }
 
--- Example/MathKitApp/MathKitApp/TransformEditor/TransformEditorView.swift
+++ Example/MathKitApp/MathKitApp/TransformEditor/TransformEditorView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 import MathKitMetal
 
-struct TransformEditorView: View {
+struct TransformEditorView<Content: View>: View {
     @State private var p0: CGPoint = .init(x: 10, y: 10)
     @State private var p1: CGPoint = .init(x: 200, y: 10)
     @State private var p2: CGPoint = .init(x: 200, y: 150)
@@ -22,15 +22,24 @@ struct TransformEditorView: View {
     @State
     private var showControlPoints = true
 
+    @State
+    private var isEditing = true
+
+    @State private var contentSize: CGSize = .zero
+
+    private let content: () -> Content
+
+    init(@ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+    }
+
     var body: some View {
-        RoundedRectangle(cornerSize: .init(width: 120, height: 120))
-            .fill(.indigo.secondary)
-            .overlay {
-                Image(systemName: "heart.circle.fill")
-                    .symbolRenderingMode(.palette)
-                    .foregroundStyle(.yellow, .green, .red.gradient)
-                    .font(.system(size: 220, weight: .semibold))
-                    .symbolEffect(.breathe)
+        content()
+            .onGeometryChange(for: CGSize.self) { proxy in
+                proxy.size
+            } action: { newValue in
+                contentSize = newValue
+                print("View size: \(contentSize)")
             }
 
             .geometryGroup()
@@ -38,7 +47,8 @@ struct TransformEditorView: View {
                 p0: p0,
                 p1: p1,
                 p2: p2,
-                p3: p3
+                p3: p3,
+                isEnabled: isEditing
             )
             .overlay {
                 TransformEditorGrid(p0: $p0, p1: $p1, p2: $p2, p3: $p3, configuration: $gridConfiguration)
@@ -54,7 +64,7 @@ struct TransformEditorView: View {
             .toolbar {
                 ToolbarItemGroup(placement: .status) {
                     Toggle(isOn: $showControlPoints) {
-                        Image(systemName: "perspective")
+                        Image(systemName: "skew")
                     }
 
                     Toggle(isOn: $gridConfiguration.showOutline) {
@@ -69,10 +79,26 @@ struct TransformEditorView: View {
                         Image(systemName: "squareshape.split.3x3")
                     }
                 }
+
+                ToolbarItemGroup(placement: .primaryAction) {
+                    Toggle(isOn: $isEditing) {
+                        Image(systemName: "perspective")
+                    }
+                }
             }
     }
 }
 
 #Preview {
-    TransformEditorView().frame(width: 300, height: 250)
+    TransformEditorView {
+        RoundedRectangle(cornerSize: .init(width: 120, height: 120))
+            .fill(.indigo.secondary)
+            .overlay {
+                Image(systemName: "heart.circle.fill")
+                    .symbolRenderingMode(.palette)
+                    .foregroundStyle(.yellow, .green, .red.gradient)
+                    .font(.system(size: 220, weight: .semibold))
+                    .symbolEffect(.breathe)
+            }.frame(width: 300, height: 250)
+    }
 }
