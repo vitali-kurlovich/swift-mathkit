d5c903ee16807eb31c7bbe79df0bac3d22c9f820
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-01-15 20:24:29 +0400

    Fix for linux

--- Sources/Benchmarks/Benchmark+MKAffineTransform.swift
+++ Sources/Benchmarks/Benchmark+MKAffineTransform.swift
@@ -78,20 +78,43 @@ extension Benchmark {
             context.blackHole(acum)
         }
 
-        benchmark.benchmark(name: "CGAffineTransform mul") { context in
-            var acum: CGFloat = 0.0
+        #if canImport(CoreGraphics)
+            benchmark.benchmark(name: "CGAffineTransform mul") { context in
+                var acum: CGFloat = 0.0
 
-            let translation = CGAffineTransform(translationX: 20, y: 30)
-            let scale = CGAffineTransform(scaleX: 4, y: 3)
-            let rotate = CGAffineTransform(rotationAngle: 45 * .pi / 180)
+                let translation = CGAffineTransform(translationX: 20, y: 30)
+                let scale = CGAffineTransform(scaleX: 4, y: 3)
+                let rotate = CGAffineTransform(rotationAngle: 45 * .pi / 180)
+
+                for _ in 0 ..< 10_000_000 {
+                    var affine = CGAffineTransform.identity
+                    affine = affine.concatenating(translation)
+                    affine = affine.concatenating(scale)
+                    affine = affine.concatenating(rotate)
+
+                    let p = CGPoint(x: 10, y: 20).applying(affine)
+
+                    acum += (p.x + p.y)
+                }
+
+                context.blackHole(acum)
+            }
+        #endif // canImport(CoreGraphics)
+
+        benchmark.benchmark(name: "MKAffineTransform mul (Double)") { context in
+            var acum = 0.0
+
+            let translation = MKAffineTransform<Double>(translationByX: 20, byY: 30)
+            let scale = MKAffineTransform<Double>(scaleByX: 4, byY: 3)
+            let rotate = MKAffineTransform<Double>(.degrees(45))
 
             for _ in 0 ..< 10_000_000 {
-                var affine = CGAffineTransform.identity
-                affine = affine.concatenating(translation)
-                affine = affine.concatenating(scale)
-                affine = affine.concatenating(rotate)
+                var affine = MKAffineTransform<Double>.identity
+                affine.append(translation)
+                affine.append(scale)
+                affine.append(rotate)
 
-                let p = CGPoint(x: 10, y: 20).applying(affine)
+                let p = affine.transform(MKPoint(x: 10, y: 20))
 
                 acum += (p.x + p.y)
             }
@@ -99,7 +122,7 @@ extension Benchmark {
             context.blackHole(acum)
         }
 
-        benchmark.benchmark(name: "MKAffineTransform mul") { context in
+        benchmark.benchmark(name: "MKAffineTransform mul (CGFloat)") { context in
             var acum: CGFloat = 0.0
 
             let translation = MKAffineTransform<CGFloat>(translationByX: 20, byY: 30)
@@ -108,6 +131,27 @@ extension Benchmark {
 
             for _ in 0 ..< 10_000_000 {
                 var affine = MKAffineTransform<CGFloat>.identity
+                affine.append(translation)
+                affine.append(scale)
+                affine.append(rotate)
+
+                let p = affine.transform(MKPoint(x: 10, y: 20))
+
+                acum += (p.x + p.y)
+            }
+
+            context.blackHole(acum)
+        }
+
+        benchmark.benchmark(name: "MKAffineTransform mul (Float16)") { context in
+            var acum: Float16 = 0.0
+
+            let translation = MKAffineTransform<Float16>(translationByX: 20, byY: 30)
+            let scale = MKAffineTransform<Float16>(scaleByX: 4, byY: 3)
+            let rotate = MKAffineTransform<Float16>(.degrees(45))
+
+            for _ in 0 ..< 10_000_000 {
+                var affine = MKAffineTransform<Float16>.identity
                 affine.append(translation)
                 affine.append(scale)
                 affine.append(rotate)
@@ -175,21 +219,21 @@ extension Benchmark {
 
     func runAffineRotateBenchmark() {
         let benchmark = BenchmarkExecuter()
+        #if canImport(CoreGraphics)
+            benchmark.benchmark(name: "CGAffineTransform rotate") { context in
+                var acum = 0.0
 
-        benchmark.benchmark(name: "CGAffineTransform rotate") { context in
-            var acum = 0.0
+                var affine = CGAffineTransform.identity
 
-            var affine = CGAffineTransform.identity
+                for _ in 0 ..< 10_000_000 {
+                    affine = affine.rotated(by: acum * (.pi / 180))
+                    acum += affine.determinant
+                }
 
-            for _ in 0 ..< 10_000_000 {
-                affine = affine.rotated(by: acum * (.pi / 180))
-                acum += affine.determinant
+                context.blackHole(acum)
             }
-
-            context.blackHole(acum)
-        }
-
-        benchmark.benchmark(name: "MKAffineTransform rotate simd") { context in
+        #endif // canImport(CoreGraphics)
+        benchmark.benchmark(name: "MKAffineTransform rotate (Double)") { context in
             var acum = 0.0
 
             var affine = MKAffineTransform<Double>.identity
