fb41f622a9300a48d7fd16ac68e713adaea461fd
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-01-16 16:05:55 +0400

    Update access levels

--- Tests/MathKitTests/Utils/ToleranceCompare.swift
+++ Tests/MathKitTests/Utils/ToleranceCompare.swift
@@ -5,10 +5,10 @@
 //  Created by Vitali Kurlovich on 24.12.25.
 //
 
-import CoreFoundation
+import Foundation
 import MathKit
 
-extension FloatingPoint {
+public extension FloatingPoint {
     func isEqual(to other: Self, tolerance: Self) -> Bool {
         // Ensure the tolerance is non-negative
         assert(tolerance >= 0)
@@ -16,56 +16,56 @@ extension FloatingPoint {
     }
 }
 
-extension CGPoint {
+public extension CGPoint {
     func isEqual(to other: Self, tolerance: CGFloat) -> Bool {
         x.isEqual(to: other.x, tolerance: tolerance) &&
             y.isEqual(to: other.y, tolerance: tolerance)
     }
 }
 
-extension CGVector {
+public extension CGVector {
     func isEqual(to other: Self, tolerance: CGFloat) -> Bool {
         dx.isEqual(to: other.dx, tolerance: tolerance) &&
             dy.isEqual(to: other.dy, tolerance: tolerance)
     }
 }
 
-extension CGSize {
+public extension CGSize {
     func isEqual(to other: Self, tolerance: CGFloat) -> Bool {
         width.isEqual(to: other.width, tolerance: tolerance) &&
             height.isEqual(to: other.height, tolerance: tolerance)
     }
 }
 
-extension CGRect {
+public extension CGRect {
     func isEqual(to other: Self, tolerance: CGFloat) -> Bool {
         origin.isEqual(to: other.origin, tolerance: tolerance) &&
             size.isEqual(to: other.size, tolerance: tolerance)
     }
 }
 
-extension MKPoint {
+public extension MKPoint {
     func isEqual(to other: Self, tolerance: Float) -> Bool {
         x.isEqual(to: other.x, tolerance: tolerance) &&
             y.isEqual(to: other.y, tolerance: tolerance)
     }
 }
 
-extension MKVector {
+public extension MKVector {
     func isEqual(to other: Self, tolerance: Float) -> Bool {
         dx.isEqual(to: other.dx, tolerance: tolerance) &&
             dy.isEqual(to: other.dy, tolerance: tolerance)
     }
 }
 
-extension MKSize {
+public extension MKSize {
     func isEqual(to other: Self, tolerance: Float) -> Bool {
         width.isEqual(to: other.width, tolerance: tolerance) &&
             height.isEqual(to: other.height, tolerance: tolerance)
     }
 }
 
-extension MKRect {
+public extension MKRect {
     func isEqual(to other: Self, tolerance: Float) -> Bool {
         origin.isEqual(to: other.origin, tolerance: tolerance) &&
             size.isEqual(to: other.size, tolerance: tolerance)
