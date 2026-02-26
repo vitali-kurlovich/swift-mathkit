7a7726ba7d277d8342e9386d5130db97922286d4
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-02-27 01:06:46 +0400

    Remove ControlPointsTransform & Example app (moved into swift-image)

--- Package.swift
+++ Package.swift
@@ -8,83 +8,54 @@ let package = Package(
     platforms: [
         .macOS(.v14),
         .iOS(.v17),
-        .watchOS(.v11),
+        .watchOS(.v10),
         .tvOS(.v17),
     ],
-    products: {
-        var products: [Product] = [
-            .library(name: "MathKit", targets: ["MathKit"]),
-            .library(name: "MathTransform", targets: ["MathTransform"]),
-        ]
-
-        #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
-            products.append(
-                .library(name: "MathKitMetal", targets: ["MathKitMetal"])
-            )
-
-        #endif // os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
-        return products
-
-    }(),
+    products: [
+        .library(name: "MathKit", targets: ["MathKit"]),
+        .library(name: "MathTransform", targets: ["MathTransform"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/vitali-kurlovich/Benchmarks", from: "0.1.6"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.7.0"),
 
     ],
-    targets: {
-        var targets: [Target] = [
-            // Targets are the basic building blocks of a package, defining a module or a test suite.
-            // Targets can depend on other targets in this package and products from dependencies.
-            .target(
-                name: "MathKit",
-                dependencies: [
-                ]
-            ),
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "MathKit",
+            dependencies: [
+            ]
+        ),
 
-            .target(
-                name: "MathTransform",
-                dependencies: ["MathKit"]
-            ),
+        .target(
+            name: "MathTransform",
+            dependencies: ["MathKit"]
+        ),
 
-            .testTarget(
-                name: "MathKitTests",
-                dependencies: [
-                    "MathKit",
-                ]
-            ),
+        .testTarget(
+            name: "MathKitTests",
+            dependencies: [
+                "MathKit",
+            ]
+        ),
 
-            .testTarget(
-                name: "MathTransformTests",
-                dependencies: [
-                    "MathKit", "MathTransform",
-                ]
-            ),
+        .testTarget(
+            name: "MathTransformTests",
+            dependencies: [
+                "MathKit", "MathTransform",
+            ]
+        ),
 
-            .executableTarget(
-                name: "MathKitBenchmarks",
-                dependencies: [
-                    "MathKit", "MathTransform",
-                    .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                    "Benchmarks",
-                ],
-                path: "Sources/Benchmarks"
-            ),
-        ]
-
-        #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
-            targets.append(
-                .target(
-                    name: "MathKitMetal",
-                    dependencies: [
-                        "MathKit",
-                    ],
-                    resources: [
-                        .process("Metal/"),
-                    ]
-                )
-            )
-        #endif // os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
-
-        return targets
-    }()
+        .executableTarget(
+            name: "MathKitBenchmarks",
+            dependencies: [
+                "MathKit", "MathTransform",
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                "Benchmarks",
+            ],
+            path: "Sources/Benchmarks"
+        ),
+    ]
 )
