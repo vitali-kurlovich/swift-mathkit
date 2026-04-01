5f07c488ac6703722e2991c27d06063f8c7fed78
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-04-01 13:21:14 +0400

    Update Benchmarks version

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .library(name: "MathTransform", targets: ["MathTransform"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vitali-kurlovich/Benchmarks", from: "0.2.0"),
+        .package(url: "https://github.com/vitali-kurlovich/Benchmarks", from: "0.3.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.7.0"),
 
     ],
