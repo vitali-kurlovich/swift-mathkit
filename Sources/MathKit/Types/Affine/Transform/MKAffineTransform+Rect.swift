ad67d202555aac926ae4ff9bc75dfb19bf677469
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-04-03 00:09:30 +0400

    Transform for center scaleMode

--- Sources/MathKit/Types/Affine/Transform/MKAffineTransform+Rect.swift
+++ Sources/MathKit/Types/Affine/Transform/MKAffineTransform+Rect.swift
@@ -2,11 +2,57 @@
 //  Created by Vitali Kurlovich on 02.04.2026.
 //
 
+public enum ScaleMode {
+    case fill
+    case aspectFit
+    case aspectFill
+    case center
+    case top
+    case bottom
+    case left
+    case right
+    case topLeft
+    case topRight
+    case bottomLeft
+    case bottomRight
+}
+
 public extension MKAffineTransform {
-    static func transform(from src: MKRect<Float>, to target: MKRect<Float>) -> Self {
+    @inlinable static func transform(from src: MKRect<Float>, to target: MKRect<Float>, scaleMode: ScaleMode = .fill) -> Self {
         assert(src.width > 0)
         assert(src.height > 0)
 
+        switch scaleMode {
+        case .fill:
+            return transformFill(from: src, to: target)
+        case .aspectFit:
+            fatalError("Not implemented")
+        case .aspectFill:
+            fatalError("Not implemented")
+        case .center:
+            return transformCenter(from: src, to: target)
+        case .top:
+            fatalError("Not implemented")
+        case .bottom:
+            fatalError("Not implemented")
+        case .left:
+            fatalError("Not implemented")
+        case .right:
+            fatalError("Not implemented")
+        case .topLeft:
+            fatalError("Not implemented")
+        case .topRight:
+            fatalError("Not implemented")
+        case .bottomLeft:
+            fatalError("Not implemented")
+        case .bottomRight:
+            fatalError("Not implemented")
+        }
+    }
+}
+
+extension MKAffineTransform {
+    @inlinable static func transformFill(from src: MKRect<Float>, to target: MKRect<Float>) -> Self {
         let scaleX = target.width / src.width
         let scaleY = target.height / src.height
 
@@ -14,5 +60,10 @@ public extension MKAffineTransform {
                      m21: 0, m22: scaleY,
                      tx: target.origin.x - scaleX * src.origin.x,
                      ty: target.origin.y - scaleY * src.origin.y)
+    }
+
+    @inlinable static func transformCenter(from src: MKRect<Float>, to target: MKRect<Float>) -> Self {
+        let offset = target.center - src.center
+        return .translation(offset)
     }
 }
--- Tests/MathKitTests/Types/AffineTransform/Transform/MKAffineTransformTests+Rect.swift
+++ Tests/MathKitTests/Types/AffineTransform/Transform/MKAffineTransformTests+Rect.swift
@@ -8,26 +8,14 @@ import Testing
 private let tolerance: Double = 0.00000001
 
 extension MKAffineTransformTests {
-    @Test("Transformation Rect Identity",
+    @Test("Transform fill",
           arguments: [
               (MKRect<Double>(x: 0, y: 0, width: 200, height: 100), MKRect<Double>(x: 0, y: 0, width: 200, height: 100)),
               (MKRect<Double>(x: 10, y: 20, width: 200, height: 100), MKRect<Double>(x: 10, y: 20, width: 200, height: 100)),
               (MKRect<Double>(x: -10, y: -20, width: 200, height: 100), MKRect<Double>(x: -10, y: -20, width: 200, height: 100)),
               (MKRect<Double>(x: 10, y: -20, width: 200, height: 100), MKRect<Double>(x: 10, y: -20, width: 200, height: 100)),
               (MKRect<Double>(x: -10, y: 20, width: 200, height: 100), MKRect<Double>(x: -10, y: 20, width: 200, height: 100)),
-          ])
-    func rectTransformationIdentity(_ args: (MKRect<Double>, MKRect<Double>)) throws {
-        let (src, dest) = args
 
-        let tr = MKAffineTransform<Double>.transform(from: src, to: dest)
-
-        #expect(tr.isEqual(to: .identity, tolerance: tolerance))
-
-        #expect(tr.transform(src).isEqual(to: dest, tolerance: tolerance))
-    }
-
-    @Test("Transformation Rect",
-          arguments: [
               (MKRect<Double>(x: 0, y: 0, width: 20, height: 10), MKRect<Double>(x: 0, y: 0, width: 20, height: 20)),
               (MKRect<Double>(x: 0, y: 0, width: 20, height: 10), MKRect<Double>(x: 0, y: 0, width: 10, height: 20)),
               (MKRect<Double>(x: 0, y: 0, width: 20, height: 10), MKRect<Double>(x: 0, y: 0, width: 20, height: 10)),
@@ -49,11 +37,38 @@ extension MKAffineTransformTests {
               (MKRect<Double>(x: -110, y: -120, width: 20, height: 10), MKRect<Double>(x: -33, y: -20, width: 80, height: 300)),
 
           ])
-    func rectTransformation(_ args: (MKRect<Double>, MKRect<Double>)) throws {
+    func fillTransform(_ args: (MKRect<Double>, MKRect<Double>)) throws {
         let (src, dest) = args
 
         let tr = MKAffineTransform.transform(from: src, to: dest)
 
         #expect(tr.transform(src).isEqual(to: dest, tolerance: tolerance))
+    }
+
+    @Test("Transform fill", arguments: [
+        (MKRect<Double>(x: 0, y: 0, width: 200, height: 100), MKRect<Double>(x: 0, y: 0, width: 200, height: 100)),
+        (MKRect<Double>(x: 10, y: 20, width: 200, height: 100), MKRect<Double>(x: 10, y: 20, width: 200, height: 100)),
+        (MKRect<Double>(x: -10, y: -20, width: 200, height: 100), MKRect<Double>(x: -10, y: -20, width: 200, height: 100)),
+        (MKRect<Double>(x: 10, y: -20, width: 200, height: 100), MKRect<Double>(x: 10, y: -20, width: 200, height: 100)),
+        (MKRect<Double>(x: -10, y: 20, width: 200, height: 100), MKRect<Double>(x: -10, y: 20, width: 200, height: 100)),
+
+        (MKRect<Double>(x: 10, y: 60, width: 20, height: 10), MKRect<Double>(x: 0, y: 0, width: 20, height: 20)),
+        (MKRect<Double>(x: -10, y: 60, width: 20, height: 510), MKRect<Double>(x: 0, y: 0, width: 10, height: 20)),
+        (MKRect<Double>(x: -50, y: -40, width: 20, height: 10), MKRect<Double>(x: 0, y: 0, width: 20, height: 10)),
+        (MKRect<Double>(x: -110, y: -120, width: 20, height: 710), MKRect<Double>(x: 0, y: 0, width: 80, height: 30)),
+
+        (MKRect<Double>(x: 10, y: 60, width: 20, height: 410), MKRect<Double>(x: 10, y: 20, width: 200, height: 200)),
+        (MKRect<Double>(x: -10, y: 60, width: 320, height: 10), MKRect<Double>(x: -10, y: 20, width: 100, height: 200)),
+        (MKRect<Double>(x: -50, y: -40, width: 20, height: 10), MKRect<Double>(x: 10, y: -20, width: 200, height: 100)),
+        (MKRect<Double>(x: -110, y: -120, width: 420, height: 10), MKRect<Double>(x: -33, y: -20, width: 80, height: 300)),
+    ])
+    func centerTransform(_ args: (MKRect<Double>, MKRect<Double>)) throws {
+        let (src, dest) = args
+
+        let tr = MKAffineTransform.transform(from: src, to: dest, scaleMode: .center)
+
+        #expect(
+            src.applying(tr).center.isEqual(to: dest.center, tolerance: tolerance)
+        )
     }
 }
