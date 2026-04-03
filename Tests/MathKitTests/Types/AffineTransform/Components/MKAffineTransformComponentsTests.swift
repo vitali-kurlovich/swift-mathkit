cc6dc7d17d9b32e25c876f94c83c3344f36a5d35
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-04-04 01:30:43 +0400

    Update tests

--- Tests/MathKitTests/Types/AffineTransform/Components/MKAffineTransformComponentsTests.swift
+++ Tests/MathKitTests/Types/AffineTransform/Components/MKAffineTransformComponentsTests.swift
@@ -65,14 +65,23 @@ extension MKAffineTransformComponentsTests {
         #expect(components.rotation.isEqual(to: .radians(.pi / 4), tolerance: tolerance))
         #expect(components.translation == .zero)
     }
+}
 
+extension MKAffineTransformComponentsTests {
     // 𝑇∗𝑅∗𝑆
     // Scale, Rotate, Translate
-    @Test("Scale-Rotate-Translate <Double>")
-    func trs_Double() {
-        let size: MKSize<Double> = .init(width: 2, height: 3)
-        let angle: MKAngle<Double> = .radians(.pi / 4)
-        let offset: MKVector<Double> = .init(dx: 20, dy: 40)
+    @Test("Scale-Rotate-Translate <Double>",
+          arguments: [
+              (MKSize<Double>.identity, MKAngle<Double>.zero, MKVector<Double>.zero),
+              (MKSize<Double>.identity, MKAngle<Double>.zero, MKVector<Double>(dx: 20, dy: 40)),
+              (MKSize<Double>.identity, MKAngle<Double>.radians(.pi / 4), MKVector<Double>(dx: 20, dy: 40)),
+              // ---
+              (MKSize<Double>(width: 2, height: 3), MKAngle<Double>.zero, MKVector<Double>.zero),
+              (MKSize<Double>(width: 2, height: 3), MKAngle<Double>.zero, MKVector<Double>(dx: 20, dy: 40)),
+              (MKSize<Double>(width: 2, height: 3), MKAngle<Double>.radians(.pi / 4), MKVector<Double>.zero),
+          ])
+    func trs_Double(_ args: (MKSize<Double>, MKAngle<Double>, MKVector<Double>)) {
+        let (size, angle, offset) = args
 
         var tr = MKAffineTransform<Double>.identity
         tr.scale(size)
@@ -82,16 +91,22 @@ extension MKAffineTransformComponentsTests {
         let components = tr.decomposed()
 
         #expect(components.scale.isEqual(to: size, tolerance: tolerance))
-        #expect(components.rotation.isEqual(to: .radians(.pi / 4), tolerance: tolerance))
-
+        #expect(components.rotation.isEqual(to: angle, tolerance: tolerance))
         #expect(components.translation.isEqual(to: offset, tolerance: tolerance))
     }
 
-    @Test("Scale-Rotate-Translate <CGFloat>")
-    func trs_CGFloat() {
-        let size: MKSize<CGFloat> = .init(width: 2, height: 3)
-        let angle: MKAngle<CGFloat> = .radians(.pi / 4)
-        let offset: MKVector<CGFloat> = .init(dx: 20, dy: 40)
+    @Test("Scale-Rotate-Translate <CGFloat>",
+          arguments: [
+              (MKSize<CGFloat>.identity, MKAngle<CGFloat>.zero, MKVector<CGFloat>.zero),
+              (MKSize<CGFloat>.identity, MKAngle<CGFloat>.zero, MKVector<CGFloat>(dx: 20, dy: 40)),
+              (MKSize<CGFloat>.identity, MKAngle<CGFloat>.radians(.pi / 4), MKVector<CGFloat>(dx: 20, dy: 40)),
+              // ---
+              (MKSize<CGFloat>(width: 2, height: 3), MKAngle<CGFloat>.zero, MKVector<CGFloat>.zero),
+              (MKSize<CGFloat>(width: 2, height: 3), MKAngle<CGFloat>.zero, MKVector<CGFloat>(dx: 20, dy: 40)),
+              (MKSize<CGFloat>(width: 2, height: 3), MKAngle<CGFloat>.radians(.pi / 4), MKVector<CGFloat>.zero),
+          ])
+    func trs_CGFloat(_ args: (MKSize<CGFloat>, MKAngle<CGFloat>, MKVector<CGFloat>)) {
+        let (size, angle, offset) = args
 
         var tr = MKAffineTransform<CGFloat>.identity
         tr.scale(size)
@@ -101,16 +116,22 @@ extension MKAffineTransformComponentsTests {
         let components = tr.decomposed()
 
         #expect(components.scale.isEqual(to: size, tolerance: tolerance))
-        #expect(components.rotation.isEqual(to: .radians(.pi / 4), tolerance: tolerance))
-
+        #expect(components.rotation.isEqual(to: angle, tolerance: tolerance))
         #expect(components.translation.isEqual(to: offset, tolerance: tolerance))
     }
 
-    @Test("Scale-Rotate-Translate <Float>")
-    func trs_Float() {
-        let size: MKSize<Float> = .init(width: 2, height: 3)
-        let angle: MKAngle<Float> = .radians(.pi / 4)
-        let offset: MKVector<Float> = .init(dx: 20, dy: 40)
+    @Test("Scale-Rotate-Translate <Float>",
+          arguments: [
+              (MKSize<Float>.identity, MKAngle<Float>.zero, MKVector<Float>.zero),
+              (MKSize<Float>.identity, MKAngle<Float>.zero, MKVector<Float>(dx: 20, dy: 40)),
+              (MKSize<Float>.identity, MKAngle<Float>.radians(.pi / 4), MKVector<Float>(dx: 20, dy: 40)),
+              // ---
+              (MKSize<Float>(width: 2, height: 3), MKAngle<Float>.zero, MKVector<Float>.zero),
+              (MKSize<Float>(width: 2, height: 3), MKAngle<Float>.zero, MKVector<Float>(dx: 20, dy: 40)),
+              (MKSize<Float>(width: 2, height: 3), MKAngle<Float>.radians(.pi / 4), MKVector<Float>.zero),
+          ])
+    func trs_Float(_ args: (MKSize<Float>, MKAngle<Float>, MKVector<Float>)) {
+        let (size, angle, offset) = args
 
         var tr = MKAffineTransform<Float>.identity
         tr.scale(size)
@@ -120,16 +141,22 @@ extension MKAffineTransformComponentsTests {
         let components = tr.decomposed()
 
         #expect(components.scale.isEqual(to: size, tolerance: halfTolerance))
-        #expect(components.rotation.isEqual(to: .radians(.pi / 4), tolerance: halfTolerance))
-
+        #expect(components.rotation.isEqual(to: angle, tolerance: halfTolerance))
         #expect(components.translation.isEqual(to: offset, tolerance: halfTolerance))
     }
 
-    @Test("Scale-Rotate-Translate <Float16>")
-    func trs_Float16() {
-        let size: MKSize<Float16> = .init(width: 2, height: 3)
-        let angle: MKAngle<Float16> = .radians(.pi / 4)
-        let offset: MKVector<Float16> = .init(dx: 20, dy: 40)
+    @Test("Scale-Rotate-Translate <Float16>",
+          arguments: [
+              (MKSize<Float16>.identity, MKAngle<Float16>.zero, MKVector<Float16>.zero),
+              (MKSize<Float16>.identity, MKAngle<Float16>.zero, MKVector<Float16>(dx: 20, dy: 40)),
+              (MKSize<Float16>.identity, MKAngle<Float16>.radians(.pi / 4), MKVector<Float16>(dx: 20, dy: 40)),
+              // ---
+              (MKSize<Float16>(width: 2, height: 3), MKAngle<Float16>.zero, MKVector<Float16>.zero),
+              (MKSize<Float16>(width: 2, height: 3), MKAngle<Float16>.zero, MKVector<Float16>(dx: 20, dy: 40)),
+              (MKSize<Float16>(width: 2, height: 3), MKAngle<Float16>.radians(.pi / 4), MKVector<Float16>.zero),
+          ])
+    func trs_Float16(_ args: (MKSize<Float16>, MKAngle<Float16>, MKVector<Float16>)) {
+        let (size, angle, offset) = args
 
         var tr = MKAffineTransform<Float16>.identity
         tr.scale(size)
@@ -139,7 +166,7 @@ extension MKAffineTransformComponentsTests {
         let components = tr.decomposed()
 
         #expect(components.scale.isEqual(to: size, tolerance: lowTolerance))
-        #expect(components.rotation.isEqual(to: .radians(.pi / 4), tolerance: 0.001))
+        #expect(components.rotation.isEqual(to: angle, tolerance: lowTolerance))
         #expect(components.translation.isEqual(to: offset, tolerance: lowTolerance))
     }
 }
--- Tests/MathKitTests/Types/AffineTransform/Components/MKAffineTransformTests+SRT.swift
+++ Tests/MathKitTests/Types/AffineTransform/Components/MKAffineTransformTests+SRT.swift
@@ -11,15 +11,21 @@ private let halfTolerance: Float32 = 0.0001
 private let lowTolerance: Float16 = 0.1
 
 extension MKAffineTransformTests {
-    @Test("Scale-Rotate-Translate <Double>")
-    func srt_Double() {
-        let size: MKSize<Double> = .init(width: 2, height: 3)
+    @Test("Scale-Rotate-Translate <Double>", arguments: [
+        (MKSize<Double>.identity, MKAngle<Double>.zero, MKVector<Double>.zero),
+        (MKSize<Double>.identity, MKAngle<Double>.zero, MKVector<Double>(dx: 20, dy: 40)),
+        (MKSize<Double>.identity, MKAngle<Double>.radians(.pi / 4), MKVector<Double>(dx: 20, dy: 40)),
+        // ---
+        (MKSize<Double>(width: 2, height: 3), MKAngle<Double>.zero, MKVector<Double>.zero),
+        (MKSize<Double>(width: 2, height: 3), MKAngle<Double>.zero, MKVector<Double>(dx: 20, dy: 40)),
+        (MKSize<Double>(width: 2, height: 3), MKAngle<Double>.radians(.pi / 4), MKVector<Double>.zero),
+
+    ])
+    func srt_Double(_ args: (MKSize<Double>, MKAngle<Double>, MKVector<Double>)) {
+        let (size, angle, offset) = args
+
         let scale: MKAffineTransform<Double> = .scale(size)
-
-        let angle: MKAngle<Double> = .radians(.pi / 4)
         let rotate: MKAffineTransform<Double> = .rotation(angle)
-
-        let offset: MKVector<Double> = .init(dx: 20, dy: 40)
         let translation: MKAffineTransform<Double> = .translation(offset)
 
         // 𝑇∗𝑅∗𝑆
@@ -38,15 +44,21 @@ extension MKAffineTransformTests {
         #expect(srt.isEqual(to: tr, tolerance: tolerance))
     }
 
-    @Test("Scale-Rotate-Translate <CGFloat>")
-    func srt_CGFloat() {
-        let size: MKSize<CGFloat> = .init(width: 2, height: 3)
+    @Test("Scale-Rotate-Translate <CGFloat>", arguments: [
+        (MKSize<CGFloat>.identity, MKAngle<CGFloat>.zero, MKVector<CGFloat>.zero),
+        (MKSize<CGFloat>.identity, MKAngle<CGFloat>.zero, MKVector<CGFloat>(dx: 20, dy: 40)),
+        (MKSize<CGFloat>.identity, MKAngle<CGFloat>.radians(.pi / 4), MKVector<CGFloat>(dx: 20, dy: 40)),
+        // ---
+        (MKSize<CGFloat>(width: 2, height: 3), MKAngle<CGFloat>.zero, MKVector<CGFloat>.zero),
+        (MKSize<CGFloat>(width: 2, height: 3), MKAngle<CGFloat>.zero, MKVector<CGFloat>(dx: 20, dy: 40)),
+        (MKSize<CGFloat>(width: 2, height: 3), MKAngle<CGFloat>.radians(.pi / 4), MKVector<CGFloat>.zero),
+
+    ])
+    func srt_CGFloat(_ args: (MKSize<CGFloat>, MKAngle<CGFloat>, MKVector<CGFloat>)) {
+        let (size, angle, offset) = args
+
         let scale: MKAffineTransform<CGFloat> = .scale(size)
-
-        let angle: MKAngle<CGFloat> = .radians(.pi / 4)
         let rotate: MKAffineTransform<CGFloat> = .rotation(angle)
-
-        let offset: MKVector<CGFloat> = .init(dx: 20, dy: 40)
         let translation: MKAffineTransform<CGFloat> = .translation(offset)
 
         // 𝑇∗𝑅∗𝑆
@@ -65,15 +77,21 @@ extension MKAffineTransformTests {
         #expect(srt.isEqual(to: tr, tolerance: tolerance))
     }
 
-    @Test("Scale-Rotate-Translate <Float>")
-    func srt_Float() {
-        let size: MKSize<Float> = .init(width: 2, height: 3)
+    @Test("Scale-Rotate-Translate <Float>", arguments: [
+        (MKSize<Float>.identity, MKAngle<Float>.zero, MKVector<Float>.zero),
+        (MKSize<Float>.identity, MKAngle<Float>.zero, MKVector<Float>(dx: 20, dy: 40)),
+        (MKSize<Float>.identity, MKAngle<Float>.radians(.pi / 4), MKVector<Float>(dx: 20, dy: 40)),
+        // ---
+        (MKSize<Float>(width: 2, height: 3), MKAngle<Float>.zero, MKVector<Float>.zero),
+        (MKSize<Float>(width: 2, height: 3), MKAngle<Float>.zero, MKVector<Float>(dx: 20, dy: 40)),
+        (MKSize<Float>(width: 2, height: 3), MKAngle<Float>.radians(.pi / 4), MKVector<Float>.zero),
+
+    ])
+    func srt_Float(_ args: (MKSize<Float>, MKAngle<Float>, MKVector<Float>)) {
+        let (size, angle, offset) = args
+
         let scale: MKAffineTransform<Float> = .scale(size)
-
-        let angle: MKAngle<Float> = .radians(.pi / 4)
         let rotate: MKAffineTransform<Float> = .rotation(angle)
-
-        let offset: MKVector<Float> = .init(dx: 20, dy: 40)
         let translation: MKAffineTransform<Float> = .translation(offset)
 
         // 𝑇∗𝑅∗𝑆
@@ -92,15 +110,21 @@ extension MKAffineTransformTests {
         #expect(srt.isEqual(to: tr, tolerance: halfTolerance))
     }
 
-    @Test("Scale-Rotate-Translate <Float16>")
-    func srt_Float16() {
-        let size: MKSize<Float16> = .init(width: 2, height: 3)
+    @Test("Scale-Rotate-Translate <Float16>", arguments: [
+        (MKSize<Float16>.identity, MKAngle<Float16>.zero, MKVector<Float16>.zero),
+        (MKSize<Float16>.identity, MKAngle<Float16>.zero, MKVector<Float16>(dx: 20, dy: 40)),
+        (MKSize<Float16>.identity, MKAngle<Float16>.radians(.pi / 4), MKVector<Float16>(dx: 20, dy: 40)),
+        // ---
+        (MKSize<Float16>(width: 2, height: 3), MKAngle<Float16>.zero, MKVector<Float16>.zero),
+        (MKSize<Float16>(width: 2, height: 3), MKAngle<Float16>.zero, MKVector<Float16>(dx: 20, dy: 40)),
+        (MKSize<Float16>(width: 2, height: 3), MKAngle<Float16>.radians(.pi / 4), MKVector<Float16>.zero),
+
+    ])
+    func srt_Float16(_ args: (MKSize<Float16>, MKAngle<Float16>, MKVector<Float16>)) {
+        let (size, angle, offset) = args
+
         let scale: MKAffineTransform<Float16> = .scale(size)
-
-        let angle: MKAngle<Float16> = .radians(.pi / 4)
         let rotate: MKAffineTransform<Float16> = .rotation(angle)
-
-        let offset: MKVector<Float16> = .init(dx: 20, dy: 40)
         let translation: MKAffineTransform<Float16> = .translation(offset)
 
         // 𝑇∗𝑅∗𝑆
