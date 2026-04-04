dbadfffc25838fb4172dad1f690e148c0ae2c72f
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-04-04 23:39:21 +0400

    Update translation in MKAffineTransform

--- Sources/MathKit/Types/Affine/MKAffineTransform+Translate.swift
+++ Sources/MathKit/Types/Affine/MKAffineTransform+Translate.swift
@@ -7,26 +7,41 @@ public extension MKAffineTransform {
      Creates an affine transformation matrix from translation values.
      The matrix takes the following form:
 
-         [ 1  0  0 ]
-         [ 0  1  0 ]
-         [ x  y  1 ]
+     [ 1  0  0 ]
+     [ 0  1  0 ]
+     [ x  y  1 ]
      */
-    @inlinable init(translationByX x: Float, byY y: Float) {
+
+    @inlinable init(translationX tx: Float, y ty: Float) {
         self.init(m11: 1, m12: 0,
                   m21: 0, m22: 1,
-                  tx: x, ty: y)
+                  tx: tx, ty: ty)
     }
 
+    @inlinable init(translationByX x: Float, byY y: Float) {
+        self.init(translationX: x, y: y)
+    }
+
+    @inlinable init(_ origin: MKPoint<Float>) {
+        self.init(translationByX: origin.x, byY: origin.y)
+    }
+
+    @inlinable init(_ vector: MKVector<Float>) {
+        self.init(translationByX: vector.dx, byY: vector.dy)
+    }
+}
+
+public extension MKAffineTransform {
     @inlinable static func translation(x: Float, y: Float) -> Self {
         .init(translationByX: x, byY: y)
     }
 
     @inlinable static func translation(_ origin: MKPoint<Float>) -> Self {
-        .translation(x: origin.x, y: origin.y)
+        .init(origin)
     }
 
     @inlinable static func translation(_ vector: MKVector<Float>) -> Self {
-        .translation(x: vector.dx, y: vector.dy)
+        .init(vector)
     }
 }
 
@@ -54,6 +69,10 @@ public extension MKAffineTransform {
 
     @inlinable func translated(x: Float, y: Float) -> Self {
         translatedBy(x: x, y: y)
+    }
+
+    @inlinable func translated(_ origin: MKPoint<Float>) -> Self {
+        translated(x: origin.x, y: origin.y)
     }
 
     @inlinable func translated(_ vector: MKVector<Float>) -> Self {
--- Tests/MathKitTests/Types/AffineTransform/MKAffineTransformTests+Translate.swift
+++ Tests/MathKitTests/Types/AffineTransform/MKAffineTransformTests+Translate.swift
@@ -2,42 +2,102 @@
 //  Created by Vitali Kurlovich on 01.04.2026.
 //
 
+import Foundation
 import MathKit
 import Testing
+
+#if canImport(CoreGraphics)
+    import CoreGraphics
+#endif
 
 private let tolerance: Double = 0.00000001
 
 extension MKAffineTransformTests {
-    @Test("Init translation")
-    func initTranslation() {
-        #expect(MKAffineTransform<Double>(translationByX: 3, byY: 6) == .init(m11: 1.0, m12: 0.0, m21: 0.0, m22: 1.0, tx: 3.0, ty: 6.0))
+    @Test("Constructor translation <Double>", arguments: [
+        (0, 0, MKAffineTransform<Double>.identity),
+        (2, 3, MKAffineTransform<Double>(m11: 1.0, m12: 0.0, m21: 0.0, m22: 1.0, tx: 2.0, ty: 3.0)),
+    ])
+    func initTranslation(_ args: (Double, Double, MKAffineTransform<Double>)) {
+        let (offsetX, offsetY, expect) = args
+        let point = MKPoint<Double>(x: offsetX, y: offsetY)
+        let vector = MKVector(dx: offsetX, dy: offsetY)
 
-        let point = MKPoint<Double>(x: 3, y: 6)
+        #expect(MKAffineTransform<Double>(translationX: offsetX, y: offsetY).isEqual(to: expect, tolerance: tolerance))
+        #expect(MKAffineTransform<Double>(translationByX: offsetX, byY: offsetY).isEqual(to: expect, tolerance: tolerance))
+        #expect(MKAffineTransform<Double>(point).isEqual(to: expect, tolerance: tolerance))
+        #expect(MKAffineTransform<Double>(vector).isEqual(to: expect, tolerance: tolerance))
 
-        #expect(MKAffineTransform<Double>(translationByX: 3, byY: 6) == .translation(point))
+        #expect(MKAffineTransform<Double>.translation(x: offsetX, y: offsetY).isEqual(to: expect, tolerance: tolerance))
+        #expect(MKAffineTransform<Double>.translation(point).isEqual(to: expect, tolerance: tolerance))
+        #expect(MKAffineTransform<Double>.translation(vector).isEqual(to: expect, tolerance: tolerance))
+
+        #if canImport(CoreGraphics)
+
+            let cg = CGAffineTransform(translationX: offsetX, y: offsetY)
+            #expect(MKAffineTransform<Double>(cg).isEqual(to: expect, tolerance: tolerance))
+
+        #endif
+
+        #if os(macOS) || os(Linux)
+            let affine = AffineTransform(translationByX: offsetX, byY: offsetY)
+            #expect(MKAffineTransform<Double>(affine).isEqual(to: expect, tolerance: tolerance))
+        #endif
     }
 
-    @Test("Translate")
-    func translate() {
-        var mk = MKAffineTransform<Double>(m11: 1, m12: 2, m21: 3, m22: 4, tx: 5, ty: 6)
+    @Test("Translate <Double>", arguments: [
+        (0, 0, MKAffineTransform<Double>.identity, MKAffineTransform<Double>.identity),
+        (2, 3, MKAffineTransform<Double>.identity, MKAffineTransform<Double>(m11: 1.0, m12: 0.0, m21: 0.0, m22: 1.0, tx: 2.0, ty: 3.0)),
 
-        let expected = MKAffineTransform<Double>(m11: 1.0, m12: 2.0, m21: 3.0, m22: 4.0, tx: 115.0, ty: 166.0)
+        (20, 30,
+         MKAffineTransform<Double>(m11: 1, m12: 2, m21: 3, m22: 4, tx: 5, ty: 6),
+         MKAffineTransform<Double>(m11: 1.0, m12: 2.0, m21: 3.0, m22: 4.0, tx: 115.0, ty: 166.0)),
 
-        let vector = MKVector<Double>(dx: 20, dy: 30)
-        let point = MKPoint<Double>(x: 20, y: 30)
-        #expect(mk.translated(vector) == expected)
+    ])
+    func translate(_ args: (Double, Double, MKAffineTransform<Double>, MKAffineTransform<Double>)) {
+        let (offsetX, offsetY, src, expect) = args
+        let point = MKPoint<Double>(x: offsetX, y: offsetY)
+        let vector = MKVector(dx: offsetX, dy: offsetY)
 
-        var copy = mk
-        copy.translate(point)
-        #expect(copy == expected)
+        #expect(
+            src.translatedBy(x: offsetX, y: offsetY).isEqual(to: expect, tolerance: tolerance)
+        )
 
-        mk.translate(vector)
+        #expect(
+            src.translated(x: offsetX, y: offsetY).isEqual(to: expect, tolerance: tolerance)
+        )
 
-        #expect(mk == expected)
+        #expect(
+            src.translated(point).isEqual(to: expect, tolerance: tolerance)
+        )
 
-        var tr = MKAffineTransform<Double>(m11: 1, m12: 2, m21: 3, m22: 4, tx: 5, ty: 6)
-        tr.prepend(.translation(vector))
+        #expect(
+            src.translated(vector).isEqual(to: expect, tolerance: tolerance)
+        )
 
-        #expect(mk == tr)
+        var tr = src
+        tr.translate(x: offsetX, y: offsetY)
+        #expect(tr.isEqual(to: expect, tolerance: tolerance))
+
+        tr = src
+        tr.translate(point)
+        #expect(tr.isEqual(to: expect, tolerance: tolerance))
+
+        tr = src
+        tr.translate(vector)
+        #expect(tr.isEqual(to: expect, tolerance: tolerance))
+
+        #if canImport(CoreGraphics)
+
+            var cg = CGAffineTransform(src)
+            cg = cg.translatedBy(x: offsetX, y: offsetY)
+            #expect(MKAffineTransform<Double>(cg).isEqual(to: expect, tolerance: tolerance))
+
+        #endif
+
+        #if os(macOS) || os(Linux)
+            var affine = AffineTransform(src)
+            affine.translate(x: offsetX, y: offsetY)
+            #expect(MKAffineTransform<Double>(affine).isEqual(to: expect, tolerance: tolerance))
+        #endif
     }
 }
