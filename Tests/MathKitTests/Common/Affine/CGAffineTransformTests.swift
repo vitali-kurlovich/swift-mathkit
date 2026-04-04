b71453c1def3bd4e92bc940236432704d836f4de
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-04-04 14:40:47 +0400

    Add CGAffineTransformTests

---
+++ Tests/MathKitTests/Common/Affine/CGAffineTransformTests.swift
@@ -0,0 +1,56 @@
+//
+//  Created by Vitali Kurlovich on 04.04.2026.
+//
+
+import Foundation
+import MathKit
+import Testing
+
+private let tolerance: Double = 0.0000000001
+private let halfTolerance: Float32 = 0.0001
+private let lowTolerance: Float16 = 0.1
+
+@Suite("CGAffineTransform")
+struct CGAffineTransformTests {}
+
+extension CGAffineTransformTests {
+    @Test("Constructor <Double>")
+    func constructorDouble() throws {
+        let tr = MKAffineTransform<Double>(m11: 1, m12: 2, m21: 3, m22: 4, tx: 5, ty: 6)
+        let affine = CGAffineTransform(tr)
+
+        let conv = MKAffineTransform<Double>(affine)
+
+        #expect(tr.isEqual(to: conv, tolerance: tolerance))
+    }
+
+    @Test("Constructor <CGFloat>")
+    func constructorCGFloat() throws {
+        let tr = MKAffineTransform<CGFloat>(m11: 1, m12: 2, m21: 3, m22: 4, tx: 5, ty: 6)
+        let affine = CGAffineTransform(tr)
+
+        let conv = MKAffineTransform<CGFloat>(affine)
+
+        #expect(tr.isEqual(to: conv, tolerance: tolerance))
+    }
+
+    @Test("Constructor <Float>")
+    func constructorFloat() throws {
+        let tr = MKAffineTransform<Float>(m11: 1, m12: 2, m21: 3, m22: 4, tx: 5, ty: 6)
+        let affine = CGAffineTransform(tr)
+
+        let conv = MKAffineTransform<Float>(affine)
+
+        #expect(tr.isEqual(to: conv, tolerance: halfTolerance))
+    }
+
+    @Test("Constructor <Float16>")
+    func constructorFloat16() throws {
+        let tr = MKAffineTransform<Float16>(m11: 1, m12: 2, m21: 3, m22: 4, tx: 5, ty: 6)
+        let affine = CGAffineTransform(tr)
+
+        let conv = MKAffineTransform<Float16>(affine)
+
+        #expect(tr.isEqual(to: conv, tolerance: lowTolerance))
+    }
+}
