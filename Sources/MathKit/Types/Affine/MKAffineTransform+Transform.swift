defef9f316e83983d4f195cc06937861ca4fa4f1
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-04-05 21:12:03 +0400

    Update transform

--- Sources/MathKit/Types/Affine/MKAffineTransform+Transform.swift
+++ Sources/MathKit/Types/Affine/MKAffineTransform+Transform.swift
@@ -18,6 +18,15 @@ public extension MKAffineTransform {
 
         return .bounds(p0, p1, p2, p3)
     }
+
+    @inlinable func transform(_ size: MKSize<Float>) -> MKSize<Float> {
+        let p0 = MKPoint(x: tx, y: ty)
+        let p1 = MKPoint(x: tx.addingProduct(m11, size.width), y: ty.addingProduct(m12, size.width))
+        let p2 = transform(MKPoint(x: size.width, y: size.height))
+        let p3 = MKPoint(x: tx.addingProduct(m21, size.height), y: ty.addingProduct(m22, size.height))
+
+        return .bounds(p0, p1, p2, p3)
+    }
 }
 
 // Inverse Transform
@@ -35,8 +44,26 @@ public extension MKPoint {
     }
 }
 
+public extension MKSize {
+    @inlinable func applying(_ t: MKAffineTransform<Float>) -> Self {
+        t.transform(self)
+    }
+}
+
 public extension MKRect {
     @inlinable func applying(_ t: MKAffineTransform<Float>) -> Self {
         t.transform(self)
+    }
+}
+
+extension MKSize {
+    @inlinable static func bounds(_ p0: MKPoint<Float>, _ p1: MKPoint<Float>, _ p2: MKPoint<Float>, _ p3: MKPoint<Float>) -> Self {
+        let minX = min(p0.x, p1.x, p2.x, p3.x)
+        let maxX = max(p0.x, p1.x, p2.x, p3.x)
+
+        let minY = min(p0.y, p1.y, p2.y, p3.y)
+        let maxY = max(p0.y, p1.y, p2.y, p3.y)
+
+        return .init(width: maxX - minX, height: maxY - minY)
     }
 }
--- Tests/MathKitTests/Types/AffineTransform/MKAffineTransformTests+Transform.swift
+++ Tests/MathKitTests/Types/AffineTransform/MKAffineTransformTests+Transform.swift
@@ -1,7 +1,4 @@
 //
-//  MKAffineTransformTests+Transform.swift
-//  swift-mathkit
-//
 //  Created by Vitali Kurlovich on 01.04.2026.
 //
 
@@ -12,36 +9,119 @@ import Testing
 private let tolerance: Double = 0.00000001
 
 extension MKAffineTransformTests {
-    @Test("Transform Point")
-    func transform_point() {
-        var mk = MKAffineTransform<Double>()
-        let point = MKPoint<Double>(x: 20, y: 40)
+    @Test("Transform Point <Double>", arguments: [
+        (MKPoint<Double>.zero, MKAffineTransform<Double>.identity, MKPoint<Double>.zero),
+        (MKPoint<Double>(x: 10, y: 20), MKAffineTransform<Double>(translationX: 2, y: 3), MKPoint<Double>(x: 12, y: 23)),
+        (MKPoint<Double>(x: 20, y: 40), MKAffineTransform<Double>(m11: 1, m12: 2, m21: 3, m22: 4, tx: 5, ty: 6), MKPoint<Double>(x: 145, y: 206)),
+        (MKPoint<Double>(x: -10, y: 20), MKAffineTransform<Double>(m11: 7, m12: 8, m21: 9, m22: 10, tx: 11, ty: 12), MKPoint<Double>(x: 121, y: 132)),
+    ])
+    func transformPointDouble(_ args: (MKPoint<Double>, MKAffineTransform<Double>, MKPoint<Double>)) {
+        let (p, tr, expect) = args
+        #expect(
+            p.applying(tr).isEqual(to: expect, tolerance: tolerance)
+        )
 
-        let cgPoint = CGPoint(x: 20, y: 40)
+        #expect(
+            tr.transform(p).isEqual(to: expect, tolerance: tolerance)
+        )
 
-        #expect(mk.transform(point) == point)
+        #if canImport(CoreGraphics)
+            let pt = CGPoint(p).applying(CGAffineTransform(tr))
+            #expect(MKPoint<Double>(pt).isEqual(to: expect, tolerance: tolerance))
+        #endif
 
-        mk.translate(x: 2, y: 5)
-        mk.rotate(.radians(.pi / 4))
-        mk.scale(x: 4, y: 3)
-        mk.rotate(.radians(.pi / 4))
-
-        #expect(point.applying(mk).isEqual(to: .init(x: -128, y: 55), tolerance: tolerance))
-        #expect(cgPoint.applying(mk).isEqual(to: .init(x: -128, y: 55), tolerance: tolerance))
-
-        let tr = MKAffineTransform<Double>(m11: 1, m12: 2, m21: 3, m22: 4, tx: 5, ty: 6)
-        #expect(point.applying(tr).isEqual(to: .init(x: 145, y: 206), tolerance: tolerance))
-
-        #expect(cgPoint.applying(tr).isEqual(to: .init(x: 145, y: 206), tolerance: tolerance))
+        #if os(macOS)
+            let affine = AffineTransform(tr)
+            let result = affine.transform(CGPoint(p))
+            #expect(MKPoint<Double>(result).isEqual(to: expect, tolerance: tolerance))
+        #endif
     }
+}
 
-    @Test("Transform Rect")
-    func transform_rect() {
-        let mk = MKAffineTransform<Double>(m11: 1, m12: 2, m21: 3, m22: 4, tx: 5, ty: 6)
-        let rect = MKRect<Double>(x: 20, y: 90, width: 200, height: 120)
-        let cgRect = CGRect(x: 20, y: 90, width: 200, height: 120)
+extension MKAffineTransformTests {
+    @Test("Transform Rect <Double>", arguments: [
+        (MKRect<Double>.zero, MKAffineTransform<Double>.identity, MKRect<Double>.zero),
+        (MKRect<Double>.identity, MKAffineTransform<Double>.identity, MKRect<Double>.identity),
 
-        #expect(rect.applying(mk).isEqual(to: .init(x: 295.0, y: 406.0, width: 560.0, height: 880.0), tolerance: tolerance))
-        #expect(cgRect.applying(mk).isEqual(to: .init(x: 295.0, y: 406.0, width: 560.0, height: 880.0), tolerance: tolerance))
+        (
+            MKRect<Double>.identity,
+            MKAffineTransform<Double>(translationX: 2, y: 3),
+            MKRect<Double>(origin: .init(x: 2, y: 3), size: .identity)
+        ),
+
+        (
+            MKRect<Double>.identity,
+            MKAffineTransform<Double>(scaleX: 2, y: 3),
+            MKRect<Double>(origin: .zero, size: .init(width: 2, height: 3))
+        ),
+
+        (
+            MKRect<Double>(x: -5, y: 10, width: 20, height: 30),
+            MKAffineTransform<Double>(m11: 7, m12: 8, m21: 9, m22: 10, tx: 11, ty: 12),
+            MKRect<Double>(x: 66, y: 72, width: 410, height: 460)
+        ),
+
+    ])
+    func transformRectDouble(_ args: (MKRect<Double>, MKAffineTransform<Double>, MKRect<Double>)) {
+        let (r, tr, expect) = args
+        #expect(
+            r.applying(tr).isEqual(to: expect, tolerance: tolerance)
+        )
+
+        #expect(
+            tr.transform(r).isEqual(to: expect, tolerance: tolerance)
+        )
+
+        #if canImport(CoreGraphics)
+            let rt = CGRect(r).applying(CGAffineTransform(tr))
+            #expect(MKRect<Double>(rt).isEqual(to: expect, tolerance: tolerance))
+        #endif
+    }
+}
+
+extension MKAffineTransformTests {
+    @Test("Transform Size <Double>", arguments: [
+        (MKSize<Double>.zero, MKAffineTransform<Double>.identity, MKSize<Double>.zero),
+        (MKSize<Double>.identity, MKAffineTransform<Double>.identity, MKSize<Double>.identity),
+
+        (
+            MKSize<Double>.identity,
+            MKAffineTransform<Double>(translationX: 2, y: 3),
+            MKSize<Double>.identity
+        ),
+
+        (
+            MKSize<Double>.identity,
+            MKAffineTransform<Double>(scaleX: 2, y: 3),
+            MKSize<Double>(width: 2, height: 3)
+        ),
+
+        (
+            MKSize<Double>(width: 20, height: 30),
+            MKAffineTransform<Double>(m11: 7, m12: 8, m21: 9, m22: 10, tx: 11, ty: 12),
+            MKSize<Double>(width: 410, height: 460)
+        ),
+
+    ])
+    func transformSizeDouble(_ args: (MKSize<Double>, MKAffineTransform<Double>, MKSize<Double>)) {
+        let (s, tr, expect) = args
+        #expect(
+            s.applying(tr).isEqual(to: expect, tolerance: tolerance)
+        )
+
+        #expect(
+            tr.transform(s).isEqual(to: expect, tolerance: tolerance)
+        )
+
+        #if canImport(CoreGraphics)
+            let st = CGSize(s).applying(CGAffineTransform(tr))
+            #expect(MKSize<Double>(st).isEqual(to: expect, tolerance: tolerance))
+        #endif
+
+        #if os(macOS)
+            let affine = AffineTransform(tr)
+            let result = affine.transform(CGSize(s))
+            #expect(MKSize<Double>(result).isEqual(to: expect, tolerance: tolerance))
+        #endif
     }
 }
