fd9d2b1770de881300542d4088b505d01ff7f290
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-09-24 07:47:28 +0400

    Update to Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,52 +1,37 @@
-// swift-tools-version: 6.3
+// swift-tools-version: 6.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
-let package = Package(
-    name: "swift-mathkit",
-    platforms: [
-        .macOS(.v14),
-        .iOS(.v16),
-        .watchOS(.v10),
-        .tvOS(.v17),
-    ],
-    products: [
-        .library(name: "MathKit", targets: ["MathKit"]),
-        .library(name: "CGMathKit", targets: ["CGMathKit"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/vitali-kurlovich/Benchmarks", from: "0.3.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.7.0"),
-        .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "MathKit",
-            dependencies: [
-            ]
-        ),
+var targets: [Target] = [
+    .target(
+        name: "MathKit",
+        dependencies: [
+        ]
+    ),
 
+    .target(
+        name: "MathKitUtils",
+        dependencies: [
+            "MathKit",
+        ]
+    ),
+
+    .testTarget(
+        name: "MathKitTests",
+        dependencies: [
+            "MathKit", "MathKitUtils",
+        ]
+    ),
+]
+
+#if os(anyAppleOS)
+
+    targets.append(contentsOf: [
         .target(
             name: "CGMathKit",
             dependencies: [
                 "MathKit",
-            ]
-        ),
-
-        .target(
-            name: "MathKitUtils",
-            dependencies: [
-                "MathKit", "CGMathKit",
-            ]
-        ),
-
-        .testTarget(
-            name: "MathKitTests",
-            dependencies: [
-                "MathKit", "MathKitUtils",
             ]
         ),
 
@@ -66,5 +51,49 @@ let package = Package(
             ],
             path: "Benchmarks"
         ),
+
+    ])
+
+#elseif os(Linux)
+    targets.append(
+        .executableTarget(
+            name: "MathKitBenchmarks",
+            dependencies: [
+                "MathKit",
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                "Benchmarks",
+            ],
+            path: "Benchmarks"
+        )
+    )
+#endif
+
+let products: [Product]
+
+#if os(anyAppleOS)
+    products = [
+        .library(name: "MathKit", targets: ["MathKit"]),
+        .library(name: "CGMathKit", targets: ["CGMathKit"]),
     ]
+#elseif os(Linux)
+    products = [
+        .library(name: "MathKit", targets: ["MathKit"]),
+    ]
+#endif
+
+let package = Package(
+    name: "swift-mathkit",
+    platforms: [
+        .macOS(.v14),
+        .iOS(.v16),
+        .watchOS(.v10),
+        .tvOS(.v17),
+    ],
+    products: products,
+    dependencies: [
+        .package(url: "https://github.com/vitali-kurlovich/Benchmarks", from: "0.3.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.7.0"),
+        .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.0.0"),
+    ],
+    targets: targets
 )
